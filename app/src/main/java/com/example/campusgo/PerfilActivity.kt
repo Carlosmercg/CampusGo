ba0ba5425f2754ee1632c373fa108f300e832657
Alejandro Caicedo <alecaice@hotmail.com>
2025-03-18 22:02:26 -0500

    Update: Se terminó la actividad de perfil

--- app/src/main/java/com/example/campusgo/PerfilActivity.kt
+++ app/src/main/java/com/example/campusgo/PerfilActivity.kt
@@ -1,21 +1,46 @@
 package com.example.campusgo
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.example.campusgo.databinding.ActivityPerfilBinding
 
 class PerfilActivity : AppCompatActivity() {
 
-    private lateinit var binding : ActivityPerfilBinding
-
+    private lateinit var binding: ActivityPerfilBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         binding = ActivityPerfilBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        binding.cuenta.setOnClickListener {
+            val intent = Intent(this, HomeActivity::class.java)
+            startActivity(intent)
+        }
+
+        binding.editarPerfil.setOnClickListener {
+            val intent = Intent(this, EditarPerfilActivity::class.java)
+            startActivity(intent)
+        }
+
+        binding.pedidos.setOnClickListener {
+            val intent = Intent(this, ListaComprasPasadasActivity::class.java)
+            startActivity(intent)
+        }
+
+        binding.mediosDePago.setOnClickListener {
+            val intent = Intent(this, PerfilMetodosDePagoActivity::class.java)
+            startActivity(intent)
+        }
+
+        binding.subirProducto.setOnClickListener {
+            val intent = Intent(this, SubirProductoActivity::class.java)
+            startActivity(intent)
+        }
+
+        binding.comprasMasRecientes.setOnClickListener {
+            val intent = Intent(this, ListaComprasPasadasActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
