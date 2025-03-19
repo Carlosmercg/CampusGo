b4773349f4879bc5fee28aad348669038cad5725
Carlosmercg <carlosdavidmercadogallego@gmail.com>
2025-03-18 21:45:38 -0500

    creacion de los mapas de comprador y vendedor

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,6 +49,7 @@ dependencies {
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+    implementation(libs.play.services.maps)
 
     // Testing
     testImplementation(libs.junit)
