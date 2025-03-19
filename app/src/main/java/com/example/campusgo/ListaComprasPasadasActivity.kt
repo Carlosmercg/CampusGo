4ad2090c52f6d37a041d12cda4f7c008a8a25c6d
Alejandro Caicedo <alecaice@hotmail.com>
2025-03-19 03:28:42 -0500

    Update: Se imlementó la lista de Compras pasadas

--- app/src/main/java/com/example/campusgo/ListaComprasPasadasActivity.kt
+++ app/src/main/java/com/example/campusgo/ListaComprasPasadasActivity.kt
@@ -5,16 +5,30 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.campusgo.adapters.CarritoAdapter
+import com.example.campusgo.databinding.ActivityListaComprasPasadasBinding
+import com.example.campusgo.models.Producto
 
 class ListaComprasPasadasActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityListaComprasPasadasBinding
+    private lateinit var carritoAdapter: CarritoAdapter
+    private val productosCarrito = mutableListOf<Producto>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_lista_compras_pasadas)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityListaComprasPasadasBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        carritoAdapter = CarritoAdapter(productosCarrito) { producto ->
+            productosCarrito.remove(producto)
+            carritoAdapter.notifyDataSetChanged()
         }
+
+
+
+        binding.recyclerCarrito.layoutManager = LinearLayoutManager(this)
+        binding.recyclerCarrito.adapter = carritoAdapter
     }
 }
