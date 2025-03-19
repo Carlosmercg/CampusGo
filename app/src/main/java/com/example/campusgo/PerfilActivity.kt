40eccf7929f771ffbcf3119d489260c3d8b2f5ad
Alejandro Caicedo <alecaice@hotmail.com>
2025-03-18 21:15:30 -0500

    Update: Se termino la vista de perfil

---
+++ app/src/main/java/com/example/campusgo/PerfilActivity.kt
@@ -0,0 +1,21 @@
+package com.example.campusgo
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.campusgo.databinding.ActivityPerfilBinding
+
+class PerfilActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityPerfilBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding = ActivityPerfilBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+    }
+}
