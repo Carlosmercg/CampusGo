038455381a841b1d9f5affe7e15fddba76bfdda9
Alejandro Caicedo <alecaice@hotmail.com>
2025-03-18 23:34:44 -0500

    Algunos cambios en EditarPerfilActivity

--- app/src/main/java/com/example/campusgo/EditarPerfilActivity.kt
+++ app/src/main/java/com/example/campusgo/EditarPerfilActivity.kt
@@ -5,16 +5,24 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.campusgo.databinding.ActivityEditarPerfilBinding
+import com.example.campusgo.databinding.ActivityPerfilBinding
 
 class EditarPerfilActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityEditarPerfilBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_editar_perfil)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityEditarPerfilBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.back.setOnClickListener {
+            finish()
         }
+
+        binding.guardar.setOnClickListener {
+            finish()
+        }
+
     }
 }
