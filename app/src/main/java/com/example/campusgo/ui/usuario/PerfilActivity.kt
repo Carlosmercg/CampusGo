75f40f8cda4b44dd96edec56b0b0c88c1df299ba
Alejandro Caicedo <alecaice@hotmail.com>
2025-06-04 02:34:43 -0500

    Update, se actualizó la lista de productos en perfil

--- app/src/main/java/com/example/campusgo/ui/usuario/PerfilActivity.kt
+++ app/src/main/java/com/example/campusgo/ui/usuario/PerfilActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.campusgo.R
 import com.example.campusgo.data.models.Pedido
 
@@ -14,9 +15,14 @@ import com.example.campusgo.ui.home.HomeActivity
 import com.example.campusgo.ui.main.BottomMenuActivity
 import com.example.campusgo.ui.producto.CrearProductoActivity
 import com.google.firebase.Firebase
+import com.example.campusgo.data.models.Producto
+import com.google.firebase.Timestamp
 import com.google.firebase.auth.auth
 import com.google.firebase.firestore.FirebaseFirestore
 import com.example.campusgo.data.repository.ManejadorImagenesAPI
+import com.example.campusgo.ui.adapters.ProductoAdapter
+import com.example.campusgo.ui.producto.DetalleProductoActivity
+import com.google.firebase.auth.FirebaseUser
 import java.text.SimpleDateFormat
 import java.util.Locale
 import java.util.Date
@@ -89,7 +95,7 @@ class PerfilActivity : BottomMenuActivity() {
                     R.drawable.ic_profile,
                 )
                 val compras = document.get("compras") as List<*>
-                if(!compras.isEmpty()) {
+                if(compras.isNotEmpty()) {
                     val compra = compras.last()
                     Log.d("PerfilActivity", "Compra: $compra")
                     inicializarCompra(compra.toString(), db)
@@ -102,37 +108,54 @@ class PerfilActivity : BottomMenuActivity() {
             .addOnFailureListener { exception ->
                 Log.e("PerfilActivity", "Error al obtener los datos del usuario", exception)
             }
+        inicializarProductos(db, usuario)
+    }
+
+    private fun inicializarProductos(db: FirebaseFirestore, usuario: FirebaseUser?){
+
+        db.collection("Productos").whereEqualTo("vendedorId", usuario?.uid ?: "").get().addOnSuccessListener { documents ->
+            val listaProductos = documents.toObjects(Producto::class.java)
+            if (listaProductos.isNotEmpty()) {
+                activarProductos()
+            }
+            val adapter = ProductoAdapter(listaProductos) { producto ->
+                Intent(this, DetalleProductoActivity::class.java).apply {
+                    putExtra("producto", producto)
+                }.also(::startActivity)
+            }
+            binding.productos.layoutManager = LinearLayoutManager(this)
+            binding.productos.adapter = adapter
+        }.addOnFailureListener { exception ->
+            Log.e("PerfilActivity", "Error al obtener los datos de los productos", exception)
+        }
+
+
     }
 
     private fun inicializarCompra(compra: String, db: FirebaseFirestore){
-        if (compra!= null) {
-            db.collection("Pedidos").document(compra).get().addOnSuccessListener { document ->
-                val pedido = document.toObject(Pedido::class.java)
-                db.collection("usuarios").document(pedido?.vendedorId ?: "").get()
-                    .addOnSuccessListener { document ->
-                        binding.comprasRecientesName.text = document.getString("nombre")
-                        pedido?.fecha?.let { timestamp ->
-                            val date = Date(timestamp.seconds * 1000) // Convert Timestamp to Date
-                            val sdf = SimpleDateFormat("d 'de' MMMM 'de' yyyy", Locale("es", "ES"))
-                            binding.comprasRecientesDate.text = sdf.format(date)
-                        } ?: run { binding.comprasRecientesDate.text = "Fecha no disponible" }
+        db.collection("Pedidos").document(compra).get().addOnSuccessListener { document ->
+            val pedido = document.toObject(Pedido::class.java)
+            db.collection("usuarios").document(pedido?.vendedorId ?: "").get()
+                .addOnSuccessListener { document ->
+                    binding.comprasRecientesName.text = document.getString("nombre")
+                    pedido?.fecha?.let { timestamp ->
+                        val date = Date(timestamp.seconds * 1000) // Convert Timestamp to Date
+                        val sdf = SimpleDateFormat("d 'de' MMMM 'de' yyyy", Locale("es", "ES"))
+                        binding.comprasRecientesDate.text = sdf.format(date)
+                    } ?: run { binding.comprasRecientesDate.text = "Fecha no disponible" }
 
-                        ManejadorImagenesAPI.mostrarImagenDesdeUrl(
-                            document.getString("urlFotoPerfil"),
-                            binding.comprasRecientesProfilePicture,
-                            this,
-                            R.drawable.ic_profile,
-                        )
-                    }.addOnFailureListener { exception ->
-                        Log.e("PerfilActivity", "Error al obtener los datos del usuario", exception)
-                        desactivarCompra()
-                    }
-            }.addOnFailureListener { exception ->
-                Log.e("PerfilActivity", "Error al obtener los datos del usuario", exception)
-                desactivarCompra()
-            }
-        }
-        else {
+                    ManejadorImagenesAPI.mostrarImagenDesdeUrl(
+                        document.getString("urlFotoPerfil"),
+                        binding.comprasRecientesProfilePicture,
+                        this,
+                        R.drawable.ic_profile,
+                    )
+                }.addOnFailureListener { exception ->
+                    Log.e("PerfilActivity", "Error al obtener los datos del usuario", exception)
+                    desactivarCompra()
+                }
+        }.addOnFailureListener { exception ->
+            Log.e("PerfilActivity", "Error al obtener los datos del usuario", exception)
             desactivarCompra()
         }
     }
@@ -143,6 +166,13 @@ class PerfilActivity : BottomMenuActivity() {
         binding.comprasRecientesProfilePicture.visibility = View.GONE
         binding.comprasRecientestextView.visibility = View.GONE
         binding.tiempo.visibility = View.GONE
+    }
+
+    private fun activarProductos(){
+        binding.empiezaAhoraTextView.visibility = View.GONE
+        binding.subirProducto.visibility = View.GONE
+        binding.textView12.visibility = View.GONE
+        binding.productos.visibility = View.VISIBLE
     }
 
 
