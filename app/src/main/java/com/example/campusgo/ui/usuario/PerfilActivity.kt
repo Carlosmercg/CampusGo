88be77b3dadf83a1316231109b2226731b331513
Alejandro Caicedo <alecaice@hotmail.com>
2025-06-04 01:16:24 -0500

    Update: funcionalidad de muestra de última compra

--- app/src/main/java/com/example/campusgo/ui/usuario/PerfilActivity.kt
+++ app/src/main/java/com/example/campusgo/ui/usuario/PerfilActivity.kt
@@ -3,7 +3,9 @@ package com.example.campusgo.ui.usuario
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import com.example.campusgo.R
+import com.example.campusgo.data.models.Pedido
 
 import com.example.campusgo.databinding.ActivityPerfilBinding
 import com.example.campusgo.ui.auth.LoginActivity
@@ -15,6 +17,9 @@ import com.google.firebase.Firebase
 import com.google.firebase.auth.auth
 import com.google.firebase.firestore.FirebaseFirestore
 import com.example.campusgo.data.repository.ManejadorImagenesAPI
+import java.text.SimpleDateFormat
+import java.util.Locale
+import java.util.Date
 
 
 class PerfilActivity : BottomMenuActivity() {
@@ -28,26 +33,13 @@ class PerfilActivity : BottomMenuActivity() {
         setContentView(binding.root)
 
         setupBottomNavigation(binding.bottomNavigation, R.id.nav_cuenta)
-        val usuario = Firebase.auth.currentUser
-        val db = FirebaseFirestore.getInstance()
-        db.collection("usuarios").document(usuario?.uid ?: "").get()
-            .addOnSuccessListener { document ->
-                val nombre = document.getString("nombre")
-                val apellido = document.getString("apellido")
-                fotoPerfil = document.getString("urlFotoPerfil")
-                binding.nombres.text = nombre + " " + apellido
-                ManejadorImagenesAPI.mostrarImagenDesdeUrl(
-                    fotoPerfil,
-                    binding.profilePicture,
-                    this,
-                    R.drawable.ic_profile,
-                )
-            }
-            .addOnFailureListener { exception ->
-                Log.e("PerfilActivity", "Error al obtener los datos del usuario", exception)
-            }
 
+        inicializarDatosUsuario()
+        inicializarListeners()
 
+    }
+
+    private fun inicializarListeners(){
         binding.back.setOnClickListener {
             val intent = Intent(this, HomeActivity::class.java)
             startActivity(intent)
@@ -80,6 +72,66 @@ class PerfilActivity : BottomMenuActivity() {
             finish()
         }
     }
+
+    private fun inicializarDatosUsuario() {
+        val usuario = Firebase.auth.currentUser
+        val db = FirebaseFirestore.getInstance()
+        db.collection("usuarios").document(usuario?.uid ?: "").get()
+            .addOnSuccessListener { document ->
+                val nombre = document.getString("nombre")
+                val apellido = document.getString("apellido")
+                fotoPerfil = document.getString("urlFotoPerfil")
+                binding.nombres.text = nombre + " " + apellido
+                ManejadorImagenesAPI.mostrarImagenDesdeUrl(
+                    fotoPerfil,
+                    binding.profilePicture,
+                    this,
+                    R.drawable.ic_profile,
+                )
+                val compras = document.get("compras") as List<*>
+                val compra = compras.last()
+                Log.d("PerfilActivity", "Compra: $compra")
+
+                inicializarCompra(compra.toString(), db)
+            }
+            .addOnFailureListener { exception ->
+                Log.e("PerfilActivity", "Error al obtener los datos del usuario", exception)
+            }
+    }
+
+    private fun inicializarCompra(compra: String, db: FirebaseFirestore){
+        if (compra!= null) {
+            db.collection("Pedidos").document(compra).get().addOnSuccessListener { document ->
+                val pedido = document.toObject(Pedido::class.java)
+                db.collection("usuarios").document(pedido?.vendedorId ?: "").get()
+                    .addOnSuccessListener { document ->
+                        binding.comprasRecientesName.text = document.getString("nombre")
+                        pedido?.fecha?.let { timestamp ->
+                            val date = Date(timestamp.seconds * 1000) // Convert Timestamp to Date
+                            val sdf = SimpleDateFormat("d 'de' MMMM 'de' yyyy", Locale("es", "ES"))
+                            binding.comprasRecientesDate.text = sdf.format(date)
+                        } ?: run { binding.comprasRecientesDate.text = "Fecha no disponible" }
+
+                        ManejadorImagenesAPI.mostrarImagenDesdeUrl(
+                            document.getString("urlFotoPerfil"),
+                            binding.comprasRecientesProfilePicture,
+                            this,
+                            R.drawable.ic_profile,
+                        )
+                    }.addOnFailureListener { exception ->
+                        Log.e("PerfilActivity", "Error al obtener los datos del usuario", exception)
+                        binding.comprasRecientesName.visibility = View.GONE
+                    }
+            }.addOnFailureListener { exception ->
+                Log.e("PerfilActivity", "Error al obtener los datos del usuario", exception)
+                binding.comprasRecientesName.visibility = View.GONE
+            }
+        }
+        else {
+            binding.comprasRecientesName.visibility = View.GONE
+        }
+    }
+
 
     override fun onResume() {
         super.onResume()
